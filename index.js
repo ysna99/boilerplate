fc0e13e21f6194a92bd6bf096153c44daf22e914
ysna99 <110143284+ysna99@users.noreply.github.com>
2024-08-18 12:58:56 +0900

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://boilerplate-rose-tau.vercel.app',
     credentials: true
 
 }
