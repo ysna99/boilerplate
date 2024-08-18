eb724a84e9c04802ac126fedeba72ba2febc8650
ysna99 <110143284+ysna99@users.noreply.github.com>
2024-08-18 11:02:12 +0900

    Update index.js

--- index.js
+++ index.js
@@ -37,4 +37,7 @@ app.get('/', (req, res, next) => {
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 
-app.listen(8000);
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
