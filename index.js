620dc24ac99c199983eaa09de97474da98222d9d
ysna99 <110143284+ysna99@users.noreply.github.com>
2024-08-18 16:33:31 +0900

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://boilerplate-one-hazel.vercel.app',
     credentials: true
 
 }
